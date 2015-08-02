9728b495b8b01937483692344fd1e0cc63118f85
seyoung <yyyyysss@naver.com>
2015-08-02 23:05:53 +0900

    Create coderbyte_CheckNums.rb

---
+++ coderbyte_CheckNums.rb
@@ -0,0 +1,19 @@
+def CheckNums(num1,num2)
+
+      
+  
+  if num1 == num2
+    return -1
+    
+  else
+    return num2 > num1
+    
+  end
+  # code goes here
+ 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+CheckNums(STDIN.gets)           
