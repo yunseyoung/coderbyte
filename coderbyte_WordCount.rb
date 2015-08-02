cd4c5c4b9c9b702ac220c6367c84a0dfee24c071
seyoung <yyyyysss@naver.com>
2015-08-02 23:08:38 +0900

    Create coderbyte_WordCount.rb

---
+++ coderbyte_WordCount.rb
@@ -0,0 +1,10 @@
+def WordCount(str)
+
+  # code goes here
+  return str.split(" ").size 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+WordCount(STDIN.gets)           
