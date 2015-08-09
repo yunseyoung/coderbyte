15791e94a9236f03ec86045a195118e2a9de65dc
seyoung <yyyyysss@naver.com>
2015-08-10 03:34:07 +0900

    Create coderbyte_Palindrome.rb

---
+++ coderbyte_Palindrome.rb
@@ -0,0 +1,13 @@
+def Palindrome(str)
+	
+  if str = str.reverse
+    return true
+  else
+    return false
+  end
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+Palindrome(STDIN.gets)        
